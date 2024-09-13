74a0e1fda50ef082030707c29f8949de566e9160
shadabdullah <shadabdullah101@gmail.com>
2024-09-13 21:33:30 +0530

    bash practice

---
+++ bash/basic.sh
@@ -0,0 +1,22 @@
+
+APPLE_PRICE=5
+echo "Price of Apple is  $APPLE_PRICE"
+echo "Price of Apple is ${APPLE_PRICE}"
+
+
+#greeting with space
+
+
+greeting="Hi     I am shad"
+
+echo "greeting with space ${greeting}\n"
+
+FILELIST=`ls`
+
+echo "$FILELIST"
+
+fileWithTimeStamp=$(date +%y-%m-%d)
+
+echo "$fileWithTimeStamp"
+
+
